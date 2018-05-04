f5f9edaed12564c211915964bcbb239538cd4643
SoonaM <SoonaM@Techtonic-MacBook-Air.local>
2018-05-04 11:22:54 -0700

    first

---
+++ getnasaapi.js
@@ -0,0 +1,27 @@
+
+
+// examples taken from https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
+
+const request = require('request');
+
+// callback based
+const baseUrl = 'https://swapi.co/api/'
+request(baseUrl + 'planets/1', { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  // console.log(res)
+  console.log(body);
+});
+
+
+
+const axios = require('axios');
+
+// promise-based
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(response => {
+  console.log(response.data.url);
+  console.log(response.data.explanation);
+})
+.catch(error => {
+  console.log(error);
+});
